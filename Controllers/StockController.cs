70084bdf6db9f06ad051d8f8005bb810555ecc02
NinaPran <70627636+NinaPran@users.noreply.github.com>
2022-09-23 09:11:05 +0200

    Add project files.

---
+++ Controllers/StockController.cs
@@ -0,0 +1,32 @@
+﻿using aksjehandel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace aksjehandel.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class StockController : ControllerBase
+    {
+        public List<Portfolio> GetAllPorfolios()
+        {
+            var portfolios = new List<Portfolio>();
+
+            var portfolio1 = new Portfolio();
+            portfolio1.Symbol = "EQNR";
+            portfolio1.Name = "EQUINOR";
+
+            var portfolio2 = new Portfolio();
+            portfolio2.Symbol = "ORKLA";
+            portfolio2.Name = "ORK";
+
+            portfolios.Add(portfolio1);
+            portfolios.Add(portfolio2);
+
+            return portfolios;
+
+        }
+        
+
+
+    }
+}
---
+++ Models/Portfolio.cs
@@ -0,0 +1,8 @@
+﻿namespace aksjehandel.Models
+{
+    public class Portfolio
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+    }
+}
