7784be6ea9598787c08ea8b57400f32b81336776
Vilde <69961376+vildekure@users.noreply.github.com>
2022-10-11 15:58:24 +0200

    Starter med inputvalidering, lagt til på Portofolio.cs

--- Models/Portfolio.cs
+++ Models/Portfolio.cs
@@ -1,9 +1,13 @@
-﻿namespace aksjehandel.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace aksjehandel.Models
 {
     public class Portfolio
     {
         public int Id { get; set; }
+        [RegularExpression(@"[0-9a-zA-ZæøåÆØÅ. \-]{2,20}")]
         public string DisplayName { get; set; }
+        [RegularExpression(@"[0-9]{2,20}")]
         public double Cash { get; set; }
         public double PurchasingPower { get; set; }
 
