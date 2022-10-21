d6f044f40ffc353796c6b8d0e397ba9420e3612c
Vilde <69961376+vildekure@users.noreply.github.com>
2022-10-21 14:07:35 +0200

    Trade class

---
+++ Models/Trade.cs
@@ -0,0 +1,14 @@
+﻿namespace aksjehandel.Models
+{
+    public class Trade
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public int BuyerPortfolioId { get; set; }
+        public int SellerPortffolioId { get; set; }
+    }
+}
