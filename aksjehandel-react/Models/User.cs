21525105bc5af0449798a08993e45d28a5f1b309
NinaPran <70627636+NinaPran@users.noreply.github.com>
2022-11-25 14:35:49 +0100

    lagt på [ExcludeFromCodeCoverage] på models

--- aksjehandel-react/Models/User.cs
+++ aksjehandel-react/Models/User.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace aksjehandel.Models
 {
     public class User
     {
+        [ExcludeFromCodeCoverage]
         [RegularExpression(@"^[a-zA-ZøæåØÆÅ. \-]{2,20}$")]
         public string Username { get; set; }
         [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$")]
