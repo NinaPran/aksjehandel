ad58c2502cd7e993dc8ee63a56bfe56b3aa915bb
Vilde <69961376+vildekure@users.noreply.github.com>
2022-11-25 14:31:37 +0100

    Home er FC

--- aksjehandel-react/client-app/src/pages/home.tsx
+++ aksjehandel-react/client-app/src/pages/home.tsx
@@ -1,4 +1,4 @@
-﻿import { Component, PropsWithChildren } from "react";
+﻿import { Component, FC, PropsWithChildren } from "react";
 import { Link } from "react-router-dom";
 import { Container, Form, FormGroup } from 'reactstrap';
 import { PortfolioSelect } from "../components/portfolio-select";
@@ -6,43 +6,25 @@ import { PortfolioContext } from "../context/portfolio-context";
 import './home.css';
 
 
+// interface HomeProps {}
 
-interface HomeProps {
-}
+export const Home: FC = () => {
+    return (
+        <Container>
+            <h1 className="header">Velkommen!</h1>
+            <Form id="mainContent">
+                <FormGroup>
+                    <PortfolioSelect></PortfolioSelect>
+                </FormGroup>
 
-interface HomeState {
-    loading: boolean;
-    error: boolean;
-}
+                <FormGroup>
+                    <Link to="/new-order" className="btn btn-primary">Ny ordre</Link>
+                </FormGroup>
 
-export class Home extends Component<HomeProps, HomeState> {
-    constructor(props: HomeProps) {
-        super(props);
-        this.state = {
-            loading: false,
-            error: false,
-        }
-    }
-
-    render() {
-        return (
-            <Container>
-                <h1 className="header">Velkommen!</h1>
-                <Form id="mainContent">
-                    <FormGroup>
-                        <PortfolioSelect></PortfolioSelect>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Link to="/new-order" className="btn btn-primary">Ny ordre</Link>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Link to="/overview" className="btn btn-primary">Min oversikt</Link>
-                    </FormGroup>
-                </Form>
-            </Container>
-        );
-    }
-
+                <FormGroup>
+                    <Link to="/overview" className="btn btn-primary">Min oversikt</Link>
+                </FormGroup>
+            </Form>
+        </Container>
+    );
 }
