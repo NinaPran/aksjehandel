fea4e986620ebddc79ee6864088ad3081f1b9dd5
Vilde <69961376+vildekure@users.noreply.github.com>
2022-11-28 14:55:07 +0100

    sign in er FC, shpuld work

--- aksjehandel-react/client-app/src/components/sign-in.tsx
+++ aksjehandel-react/client-app/src/components/sign-in.tsx
@@ -1,4 +1,4 @@
-﻿import React, { Component, createRef, PropsWithChildren, useRef } from "react";
+﻿import React, { Component, createRef, FC, PropsWithChildren, useRef, useState } from "react";
 import { Container } from 'reactstrap';
 import { Portfolio } from "../types/portfolio";
 import { Form, FormGroup } from 'reactstrap';
@@ -8,119 +8,99 @@ interface SignInProps {
     onSignedIn: () => void;
 }
 
-interface SignInState {
-    usernameError: string;
-    passwordError: string;
-    signInError: string;
-
+interface validateUserProps {
+    onUserValid: () => boolean;
 }
 
-export class SignIn extends Component<SignInProps, SignInState> {
-    usernameref = createRef<HTMLInputElement>();
-    passwordref = createRef<HTMLInputElement>();
-    constructor(props: SignInProps) {
-        super(props);
-        this.state = {
-            usernameError: "",
-            passwordError: "",
-            signInError: "",
-        }
+export const SignIn: FC<SignInProps> = (props) => {
+    const [usernameError, setUsernameError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [signInError, setSignInError] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const loginclick = (e: any) => {
+        const searchParams = new URLSearchParams();
+        searchParams.append('username', username);
+        searchParams.append('password', password);
+
+        setSignInError("");
+
+        fetch('stock/SignIn', {
+            method: 'post',
+            body: searchParams
+        })
+            .then(response => response.json())
+            .then(response => {
+                if (response == true) {
+                    props.onSignedIn();
+                }
+                else {
+                    setSignInError("Feil brukernavn eller passord");
+                }
+
+            })
+            .catch(error => {
+                setSignInError("Feil på server -prøv igjen senere");
+            });
     }
 
-    loginclick = () => {
-        const usernameOk = this.validateUser();
-        const passwordOk = this.validatePassword();
-
-        if (usernameOk && passwordOk) {
-            const username = this.usernameref.current?.value || '';
-            const password = this.passwordref.current?.value || '';
-            const searchParams = new URLSearchParams();
-            searchParams.append('username', username);
-            searchParams.append('password', password);
-            this.setState({
-                signInError: ""
-            })
-            fetch('stock/SignIn', {
-                method: 'post',
-                body: searchParams
-            })
-                .then(response => response.json())
-                .then(response => {
-                    if (response == true) {
-                        this.props.onSignedIn();
-                    }
-                    else {
-                        this.setState({
-                            signInError: "Feil brukernavn eller passord"
-                        })
-                    }
-
-                })
-                .catch(error => this.setState({
-                    signInError: "Feil på server -prøv igjen senere"
-                }));
-        }
-    }
-
-    validateUser = () => {
+    const validateUser = (event: React.ChangeEvent<HTMLInputElement> | undefined) => {
         const regexp = /^[a-zA-ZæøåÆØÅ\.\-]{2,20}$/;
-        const ok = regexp.test(this.usernameref.current?.value || "");
+        const ok = event && regexp.test(event.target.value || "");
+
         if (!ok) {
-            this.setState({ usernameError: "Brukernavnet må bestå av 2 til 20 bokstaver" });
+            setUsernameError("Brukernavnet må bestå av 2 til 20 bokstaver");
             return false;
         }
         else {
-            this.setState({ usernameError: "" });
+            setUsernameError("");
+            setUsername(event.target.value);
             return true;
         }
 
     }
 
-    validatePassword = () => {
+    const validatePassword = (event: React.ChangeEvent<HTMLInputElement> | undefined) => {
         const regexp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
-        const ok = regexp.test(this.passwordref.current?.value || "");
+        const ok = event && regexp.test(event.target.value || "");
+
         if (!ok) {
-            this.setState({ passwordError: "Passordet må bestå av minimum 6 tegn, minst en bokstav og et tall" });
+            setPasswordError("Passordet må bestå av minimum 6 tegn, minst en bokstav og et tall");
             return false;
         }
         else {
-            this.setState({ passwordError: "" });
+            setPasswordError("");
+            setPassword(event.target.value);
             return true;
         }
 
     }
+    return (
+        <div className="container">
+            <h1 className="header" style={{ marginTop: "30px" }}>Logg inn</h1>
+            <Form>
 
+                <FormGroup>
+                    <label style={{ marginRight: "10px" }}>Brukernavn</label>
+                    <input onChange={validateUser} type="text" />
+                    <span style={{ color: "red" }}>{usernameError}</span>
+                </FormGroup>
 
-    render() {
-        const { } = this.props;
-        const { usernameError, passwordError, signInError } = this.state;
+                <FormGroup>
+                    <label style={{ marginRight: "34px" }}>Passord</label>
+                    <input onChange={validatePassword} type="password" />
+                    <span style={{ color: "red" }}>{passwordError}</span>
+                </FormGroup>
 
-        return (
-            <div className="container">
-                <h1 className="header" style={{ marginTop: "30px" }}>Logg inn</h1>
-                <Form>
+                <FormGroup>
+                    <input type="button" value="Logg inn" onClick={loginclick} className="btn btn-primary" />
+                </FormGroup>
 
-                    <FormGroup>
-                        <label style={{ marginRight: "10px" }}>Brukernavn</label>
-                        <input ref={this.usernameref} onChange={this.validateUser} type="text" />
-                        <span style={{ color: "red" }}>{usernameError}</span>
-                    </FormGroup>
+                <div style={{ color: "red" }}>{signInError}</div>
 
-                    <FormGroup>
-                        <label style={{ marginRight: "34px" }}>Passord</label>
-                        <input ref={this.passwordref} onChange={this.validatePassword} type="password" />
-                        <span style={{ color: "red" }}>{passwordError}</span>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <input type="button" value="Logg inn" onClick={this.loginclick} className="btn btn-primary" />
-                    </FormGroup>
-
-                    <div style={{ color: "red" }}>{signInError}</div>
-
-                </Form>
-            </div>
-        );
-    }
-
+            </Form>
+        </div>
+    );
 }
